0c6a4354ade24e3fcd8ff3d68d1e35cb035a1582
unndreay <andre.presser@rl-institut.de>
2020-05-04 12:38:31 +0200

    Use scripts from use case 4d as a template (newest in dev)

---
+++ experiment_1/scripts/FlexMex1_2b/runall.py
@@ -0,0 +1,18 @@
+import logging
+
+import preprocessing
+import infer
+import optimization
+import postprocessing
+
+
+if __name__ == '__main__':
+    logging.info("Start running the whole modelling pipeline")
+
+    preprocessing.main()
+
+    infer.main()
+
+    optimization.main()
+
+    postprocessing.main()
