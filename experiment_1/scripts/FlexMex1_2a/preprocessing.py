60d7b715f31d86d7201106d55ffd950b0b532f69
unndreay <andre.presser@rl-institut.de>
2020-05-28 15:01:36 +0200

    Revert the last 4 commits e41e308c3be4b443c0a2937de1985b82bb9377b3 0eccbc3bc5935ee02d7301d95aa31ebad8c4db30 392ef4c6146805e749df9c2c61eae52fcef4f805 04b903551a8e6589b6d338c60f129ffec1c16759

--- experiment_1/scripts/FlexMex1_2a/preprocessing.py
+++ experiment_1/scripts/FlexMex1_2a/preprocessing.py
@@ -52,7 +52,7 @@ def main():
             'wind-onshore',
             'solar-pv',
             'uranium-nuclear-st',
-            # 'ch4-gt'
+            'ch4-gt'
         ]
     )
 
@@ -63,7 +63,7 @@ def main():
     update_wind_offshore(exp_paths.data_preprocessed, scalars)
     update_solar_pv(exp_paths.data_preprocessed, scalars)
     update_nuclear_st(exp_paths.data_preprocessed, scalars, expandable=True, from_green_field=True)
-    # update_ch4_gt(exp_paths.data_preprocessed, scalars, expandable=True, from_green_field=True)
+    update_ch4_gt(exp_paths.data_preprocessed, scalars, expandable=True, from_green_field=True)
 
     # create sequences
     create_electricity_demand_profiles(exp_paths.data_raw, exp_paths.data_preprocessed)
@@ -74,7 +74,7 @@ def main():
     # compare with previous data
     previous_path = os.path.join(os.path.split(exp_paths.data_preprocessed)[0] + '_default', 'data')
     new_path = exp_paths.data_preprocessed
-    # check_if_csv_dirs_equal(new_path, previous_path)
+    check_if_csv_dirs_equal(new_path, previous_path)
 
 
 if __name__ == '__main__':
