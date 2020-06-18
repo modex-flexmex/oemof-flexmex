d88530c4efd48d509b23d7ce0946e08c003bb37c
jnnr <jann.launer@rl-institut.de>
2020-06-18 10:06:54 +0200

    Write lp file and print results

--- tests/test_facades.py
+++ tests/test_facades.py
@@ -1,7 +1,7 @@
+import os
 import pandas as pd
 
-from oemof.network import Node
-from oemof.solph import Bus, Source, Flow, EnergySystem, Model
+from oemof.solph import Bus, Sink, Source, Flow, EnergySystem, Model
 from oemoflex.facades import Bev
 
 
@@ -9,16 +9,21 @@ def test_bev():
     timeindex = pd.date_range('2020-01-01', periods=3, freq='H')
 
     es = EnergySystem(timeindex=timeindex)
-    Node.registry = es
+    # Node.registry = es
 
     el_bus = Bus(label='electricity')
 
-    Source(
+    el_grid = Source(
         label='el_grid',
         outputs={el_bus: Flow(variable_costs=120)}
     )
 
-    Bev(
+    el_excess = Sink(
+        label='el_excess',
+        inputs={el_bus: Flow(variable_costs=0.0001)}
+    )
+
+    bev = Bev(
         label='my_bev',
         bus=el_bus,
         carrier='electricity',
@@ -26,16 +31,31 @@ def test_bev():
         storage_capacity=1000,
         capacity=50,
         availability=[0.8, 0.7, 0.6],  # GridAvailabilityRate
-        drive_power=[0.01, 0.02, 0.006],  # DrivePower
-        amount=1000,  # AnnualDemand
+        drive_power=[0.01, 0.02, 0.022],  # DrivePower
+        amount=100,  # AnnualDemand
         loss_rate=0.01,
-        initial_storage_level=0.5,
+        # initial_storage_level=0.5,
         min_storage_level=[0.1, 0.2, 0.15],  # MinBatteryLevel
         max_storage_level=[0.9, 0.95, 0.92],  # MaxBatteryLevel
         efficiency=0.93
     )
 
+    es.add(el_bus, el_grid, el_excess, bev)
+
     m = Model(es)
+
+    lp_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lp-file.lp')
+    m.write(lp_file_path, io_options={'symbolic_solver_labels': True})
+
     m.solve()
 
-    m.results()
+    results = m.results()
+
+    seq_dict = {k: v['sequences'] for k, v in results.items() if 'sequences' in v}
+    sequences = pd.concat(seq_dict.values(), 1)
+    sequences.columns = seq_dict.keys()
+    print(sequences)
+
+
+if __name__ == '__main__':
+    test_bev()
