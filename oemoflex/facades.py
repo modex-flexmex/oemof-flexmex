4995aabc6459fafeb2657aa82d707f8e304a3e93
jnnr <jann.launer@rl-institut.de>
2020-09-17 16:58:22 +0200

    Break lines

--- oemoflex/facades.py
+++ oemoflex/facades.py
@@ -487,7 +487,11 @@ class ReservoirWithPump(GenericStorage, Facade):
         inflow = Source(
             label=self.label + "-inflow",
             outputs={
-                internal_bus: Flow(nominal_value=self.capacity_turbine, max=self.profile, fixed=False)
+                internal_bus: Flow(
+                    nominal_value=self.capacity_turbine,
+                    max=self.profile,
+                    fixed=False
+                )
             },
             carrier=self.carrier,
             tech=self.tech
@@ -510,4 +514,9 @@ class ReservoirWithPump(GenericStorage, Facade):
         self.subnodes = (inflow, internal_bus, pump)
 
 
-TYPEMAP.update({"asymmetric storage": AsymmetricStorage, "reservoir": ReservoirWithPump, "bev": Bev})
+TYPEMAP.update(
+    {
+        "asymmetric storage": AsymmetricStorage,
+        "reservoir": ReservoirWithPump, "bev": Bev
+    }
+)
