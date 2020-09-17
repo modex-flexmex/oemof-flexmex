656d6ff03aaa9e147a7aa10fbb358228db051edc
unndreay <andre.presser@rl-institut.de>
2020-09-17 16:09:27 +0200

    Update to recent Scalars.csv

--- experiment_1/scripts/FlexMex1_2d/preprocessing.py
+++ experiment_1/scripts/FlexMex1_2d/preprocessing.py
@@ -34,7 +34,7 @@ def main():
     # Load common input parameters
     scalars = pd.read_csv(
         os.path.join(exp_paths['data_raw'], 'Scalars.csv'),
-        sep=';',
+        sep=',',
         header=0,
         na_values=['not considered', 'no value']
     )
