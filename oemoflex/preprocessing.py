e0becac2cc70a8b96230830f2e682eeaf7b85677
unndreay <andre.presser@rl-institut.de>
2020-11-12 13:10:48 +0100

    Reformat components.csv and read-in function * csv: Drop columns repeated in the actual definition * csv: Include path to the definition * func: Restructure path operations * func: Add path read-in from csv

--- oemoflex/preprocessing.py
+++ oemoflex/preprocessing.py
@@ -24,7 +24,6 @@ def create_default_elements(
         dir,
         busses_file=os.path.join(module_path, 'model_structure', 'busses.csv'),
         components_file=os.path.join(module_path, 'model_structure', 'components.csv'),
-        component_attrs_dir=os.path.join(module_path, 'model_structure', 'component_attrs'),
         select_components=None,
 ):
     r"""
@@ -50,29 +49,32 @@ def create_default_elements(
     -------
     None
     """
-    components_file = os.path.join(module_path, components_file)
+    components_data = pd.read_csv(components_file)
 
-    # TODO Better put this as another field into the components.csv as well?
-    component_attrs_dir = os.path.join(module_path, component_attrs_dir)
+    components_dirname = os.path.dirname(components_file)
 
-    defined_components = pd.read_csv(components_file).name.values
+    defined_components_names = components_data['name'].values
+    defined_components = components_data
 
     if select_components is None:
-        select_components = defined_components
+        select_components = defined_components_names
 
-    for component in select_components:
+    for component_name in select_components:
 
-        if component not in defined_components:
+        if component_name not in defined_components_names:
             raise ValueError(
-                f"Selected component '{component}' not found in components definitions."
+                f"Selected component '{component_name}' not found in components definitions."
             )
 
-        component_attrs_file = os.path.join(component_attrs_dir, component + '.csv')
+        component_data = defined_components.loc[defined_components['name'] == component_name]
+        component_path = component_data['path'].values[0]
+
+        component_attrs_file = os.path.join(components_dirname, component_path)
 
         df = create_component_element(component_attrs_file)
 
         # Write to target directory
-        df.to_csv(os.path.join(dir, component + '.csv'))
+        df.to_csv(os.path.join(dir, component_name + '.csv'))
 
     bus_df = create_bus_element(busses_file)
     bus_df.to_csv(os.path.join(dir, 'bus.csv'))
