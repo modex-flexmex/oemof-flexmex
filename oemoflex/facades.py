f3e3e1d3ac47461fd261601aedbdbea0404ceec4
unndreay <andre.presser@rl-institut.de>
2020-12-16 11:30:15 +0100

    Revert "Fix an 'E0203 access-member-before-definition' pylint unhappiness"
    This reverts commit 3546b6a02148369b71dc7f3abb3d5350287a7845.
    
    Too hasty. Built in an error here. Pylint message was misleading.

--- oemoflex/facades.py
+++ oemoflex/facades.py
@@ -657,9 +657,9 @@ class ExtractionTurbine(ExtractionTurbineCHP, Facade):  # pylint: disable=too-ma
 
         self.capacity = kwargs.get("capacity")
 
-        self.condensing_efficiency = sequence(self.condensing_efficiency)
-
         self.fuel_capacity = self.capacity / self.condensing_efficiency
+
+        self.condensing_efficiency = sequence(self.condensing_efficiency)
 
         self.marginal_cost = kwargs.get("marginal_cost", 0)
 
