0aefa5198443db738971b4f8c8f02f8a15b703ae
jnnr <jann.launer@rl-institut.de>
2021-01-26 16:05:04 +0100

    Remove unused functions

--- oemoflex/postprocessing_sketch.py
+++ oemoflex/postprocessing_sketch.py
@@ -4,100 +4,8 @@ import numpy as np
 import pandas as pd
 
 from oemof.solph import Bus, EnergySystem
-from oemof.outputlib import processing
 
 from oemoflex.postprocessing import create_postprocessed_results_subdirs
-
-
-def get_flow_from_oemof_tuple(oemof_tuple):
-    r"""
-    Returns the flow object for a given oemof tuple.
-
-    Parameters
-    ----------
-    oemof_tuple : tuple
-        Tuple of type (bus, component, xx)
-
-    Returns
-    -------
-    flow : oemof.solph.Flow
-        Flow object corresponding to the tuple
-    """
-    if isinstance(oemof_tuple[0], Bus):
-        component = oemof_tuple[1]
-        bus = oemof_tuple[0]
-
-    elif isinstance(oemof_tuple[1], Bus):
-        component = oemof_tuple[0]
-        bus = oemof_tuple[1]
-
-    else:
-        return None
-
-    flow = component.outputs[bus]
-
-    return flow
-
-
-def select_from_dict(dict, name):
-    r"""
-    Returns
-    Parameters
-    ----------
-    dict
-    name
-
-    Returns
-    -------
-
-    """
-    def has_var_name(v, name):
-        return (name in v['scalars'].index) or (name in v['sequences'].columns)
-
-    def get_var_value(v, name):
-        if name in v['scalars'].index:
-            return v['scalars'][name]
-        elif name in v['sequences'].columns:
-            return v['sequences'][name]
-
-    selected_param_dict = copy.deepcopy(
-        {
-            k: get_var_value(v, name)
-            for k, v in dict.items()
-            if has_var_name(v, name)
-         }
-    )
-
-    return selected_param_dict
-
-
-def multiply_param_with_variable(params, results, param_name, var_name):
-    def get_label(k):
-        if isinstance(k, tuple):
-            return tuple(map(str, k))
-        return str(k)
-
-    parameter = select_from_dict(params, param_name)
-
-    variable = select_from_dict(results, var_name)
-
-    intersection = (
-        processing.convert_keys_to_strings(parameter).keys()
-        & processing.convert_keys_to_strings(variable).keys()
-    )
-
-    product = {}
-    for k, var in variable.items():
-        if get_label(k) in intersection:
-            par = processing.convert_keys_to_strings(parameter)[get_label(k)]
-
-            if isinstance(par, pd.Series):
-                par.index = var.index
-
-            prod = var * par
-            product.update({k: prod})
-
-    return product
 
 
 def get_sequences(dict):
@@ -118,17 +26,6 @@ def get_scalars(dict):
     return scalars
 
 
-def sum_sequences(sequences):
-
-    _sequences = copy.deepcopy(sequences)
-
-    for oemof_tuple, value in _sequences.items():
-
-        _sequences[oemof_tuple] = value.sum()
-
-    return _sequences
-
-
 def get_component_from_oemof_tuple(oemof_tuple):
     if isinstance(oemof_tuple[1], Bus):
         component = oemof_tuple[0]
@@ -143,23 +40,6 @@ def get_component_from_oemof_tuple(oemof_tuple):
         component = oemof_tuple[1]
 
     return component
-
-
-def filter_components_by_attr(sequences, **kwargs):
-
-    filtered_seqs = {}
-
-    for oemof_tuple, data in sequences.items():
-        component = get_component_from_oemof_tuple(oemof_tuple)
-
-        for key, value in kwargs.items():
-            if not hasattr(component, key):
-                continue
-
-            if getattr(component, key) in value:
-                filtered_seqs[oemof_tuple] = data
-
-    return filtered_seqs
 
 
 def filter_series_by_component_attr(df, **kwargs):
