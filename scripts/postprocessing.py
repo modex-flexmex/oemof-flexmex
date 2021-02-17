c18ac94f32b71265e2f31f655315624b1807ca48
unndreay <andre.presser@rl-institut.de>
2021-02-17 16:33:53 +0100

    Update postprocessing script * Use Dict() for the moment to mimic 'exp_paths' structure

--- scripts/postprocessing.py
+++ scripts/postprocessing.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from addict import Dict
+
 from oemof.tools.logger import define_logging
 
 from oemoflex.postprocessing import run_postprocessing
@@ -10,6 +12,13 @@ from oemoflex.helpers import setup_experiment_paths, check_if_csv_dirs_equal, lo
 if __name__ == '__main__':
     scenario_specs = load_yaml(sys.argv[1])
 
+    paths = Dict()
+    paths.data_raw = sys.argv[2]
+    paths.data_preprocessed = sys.argv[3]
+    paths.results_optimization = sys.argv[4]
+    paths.results_template = sys.argv[5]
+    paths.results_postprocessed = sys.argv[6]
+
     exp_paths = setup_experiment_paths(scenario_specs['scenario'])
 
     logpath = define_logging(
@@ -17,11 +26,11 @@ if __name__ == '__main__':
         logfile='oemoflex.log'
     )
 
-    run_postprocessing(scenario_specs, exp_paths)
+    run_postprocessing(scenario_specs, paths)
 
     # compare with previous data
-    previous_path = exp_paths.results_postprocessed.replace('results', 'defaults')
-    new_path = exp_paths.results_postprocessed
+    previous_path = paths.results_postprocessed.replace('results', 'defaults')
+    new_path = paths.results_postprocessed
     try:
         check_if_csv_dirs_equal(new_path, previous_path)
     except AssertionError as e:
