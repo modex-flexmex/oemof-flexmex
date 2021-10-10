972c51c3a1ac98d5976e31bb3b74ae3b508111de
monika <monika@orl25.de>
2021-10-10 16:11:07 +0200

    Move plot generation into a function

--- scripts/plot_dispatch.py
+++ scripts/plot_dispatch.py
@@ -18,6 +18,50 @@ def sum_demands(data, bus_name, demand_name):
     data[(bus_name, bus_name + '-demand', 'flow')] = total_demand
     return data
 
+
+# normal dispatch plot
+# plot one winter and one summer month
+def draw_plots(df, start_date, end_date, bus_name, colors_odict):
+    fig, ax = plt.subplots(figsize=(12, 5))
+
+    # filter timeseries
+    df_time_filtered = plots.filter_timeseries(df, start_date, end_date)
+    df_demand_time_filtered = plots.filter_timeseries(
+        df_demand, start_date, end_date
+    )
+    # plot time filtered data
+    plots.plot_dispatch(
+        ax=ax, df=df_time_filtered, df_demand=df_demand_time_filtered, unit="W", colors_odict=colors_odict
+    )
+
+    plt.grid()
+    plt.title(bus_name + " dispatch", pad=20, fontdict={"size": 22})
+    plt.xlabel("Date", loc="right", fontdict={"size": 17})
+    plt.ylabel("Power", loc="top", fontdict={"size": 17})
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    # format x-axis representing the dates
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+    plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
+
+    # Shrink current axis's height by 10% on the bottom
+    box = ax.get_position()
+    ax.set_position(
+        [box.x0, box.y0 + box.height * 0.15, box.width, box.height * 0.85]
+    )
+    # Put a legend below current axis
+    ax.legend(
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        fancybox=True,
+        ncol=4,
+        fontsize=14,
+    )
+
+    fig.tight_layout()
+    return fig
+
+
 if __name__ == "__main__":
 
     paths = Dict()
@@ -41,7 +85,7 @@ if __name__ == "__main__":
 
 
     selected_timeseries_files = [file for file in timeseries_files for carrier in carriers if carrier in file]
-    print(selected_timeseries_files)
+
     for bus_file in selected_timeseries_files:
 
         bus_name = os.path.splitext(bus_file)[0]
@@ -65,6 +109,7 @@ if __name__ == "__main__":
             unit="W",
             colors_odict=colors_odict
         )
+
         file_name = bus_name + "_dispatch_interactive" + ".html"
         fig_plotly.write_html(
             file=os.path.join(paths.plotted, file_name),
@@ -72,46 +117,9 @@ if __name__ == "__main__":
             # full_html=False
         )
 
-        # normal dispatch plot
-        # plot one winter and one summer month
+
         for start_date, end_date in timeframe:
-            fig, ax = plt.subplots(figsize=(12, 5))
-
-            # filter timeseries
-            df_time_filtered = plots.filter_timeseries(df, start_date, end_date)
-            df_demand_time_filtered = plots.filter_timeseries(
-                df_demand, start_date, end_date
-            )
-            # plot time filtered data
-            plots.plot_dispatch(
-                ax=ax, df=df_time_filtered, df_demand=df_demand_time_filtered, unit="W", colors_odict=colors_odict
-            )
-
-            plt.grid()
-            plt.title(bus_name + " dispatch", pad=20, fontdict={"size": 22})
-            plt.xlabel("Date", loc="right", fontdict={"size": 17})
-            plt.ylabel("Power", loc="top", fontdict={"size": 17})
-            plt.xticks(fontsize=14)
-            plt.yticks(fontsize=14)
-            # format x-axis representing the dates
-            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-            plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
-
-            # Shrink current axis's height by 10% on the bottom
-            box = ax.get_position()
-            ax.set_position(
-                [box.x0, box.y0 + box.height * 0.15, box.width, box.height * 0.85]
-            )
-            # Put a legend below current axis
-            ax.legend(
-                loc="upper center",
-                bbox_to_anchor=(0.5, -0.1),
-                fancybox=True,
-                ncol=4,
-                fontsize=14,
-            )
-
-            fig.tight_layout()
+            fig = draw_plots(df=df, start_date=start_date, end_date=end_date, bus_name=bus_name, colors_odict=colors_odict)
             file_name = bus_name + "_" + start_date[5:7] + ".pdf"
             plt.savefig(os.path.join(paths.plotted, file_name), bbox_inches="tight")
             file_name = bus_name + "_" + start_date[5:7] + ".png"
