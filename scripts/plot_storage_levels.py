ed28c806e02daeef8a6a0265adbdf35f3c99003a
monika <monika@orl25.de>
2021-10-19 13:43:02 +0200

    Draw first (incomplete) plots

--- scripts/plot_storage_levels.py
+++ scripts/plot_storage_levels.py
@@ -1,14 +1,21 @@
 import os
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 from addict import Dict
+import oemoflex.tools.plots as plots
+import oemoflex.tools.helpers as helpers
+
 
 # Goal: In every scenario two png-files for each selected country (with selection being the same as in the dispatch
 # plots), which display the storage level change in the two timeframes selected for the dispatch plots. The file should
 # also use the colors and the labels files that are used in the dispatch plots.
 
-# first step: data import
 
+dir_name = os.path.abspath(os.path.dirname(__file__))
+labels_dict = helpers.load_yaml(os.path.join(dir_name, "../oemof_flexmex/model_config/plot_labels.yml"))
+
+# first step: data import
 paths = Dict()
 paths.postprocessed = sys.argv[1]
 paths.plotted = sys.argv[2]
@@ -16,7 +23,7 @@ if not os.path.exists(paths.plotted):
         os.makedirs(paths.plotted)
 timeseries_directory = os.path.join(paths.postprocessed, "oemoflex-timeseries/variable")
 capacity_data = os.path.join(timeseries_directory, "capacity.csv")
-capacities = pd.read_csv(capacity_data, parse_dates=[0], index_col=[0])
+capacities = pd.read_csv(capacity_data, header=[0, 1, 2], parse_dates=[0], index_col=[0])
 
 # second step: data selection: timeframe, country
 #TODO: integrate time frame and region definition for dispatch plots and for storage level plots
@@ -29,6 +36,20 @@ regions = ["DE", "FR", "PL"]
 for region in regions:
     df = pd.DataFrame()
     for column in capacities.columns:
-        if region in column:
+        if region in column[0]:
             df[column] = capacities.loc[:, column]
-    print(df)
+
+# third step: rename columns into short, understandable labels
+
+    df.columns = plots._rename_by_string_matching(columns=df.columns, labels_dict=labels_dict)
+    fig, ax = plt.subplots(figsize=(14, 5), linewidth=20)
+
+# fourth step: slice selected timeframes
+
+# fifth step: plot
+    ax1 = plt.subplot(2, 1, 1)
+    ax3 = plt.subplot(2, 1, 2, sharex=ax1)
+    for i in df.columns:
+        ax1.plot(df.index, df[i], label=i, linewidth=2)#, color=colors_odict[i])
+    ax1.legend()
+    plt.show()
