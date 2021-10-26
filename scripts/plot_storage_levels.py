743fc44b63bef121d00474b5cc574f80e9ebb95f
monika <monika@orl25.de>
2021-10-26 12:54:17 +0200

    Test commit for CI

--- scripts/plot_storage_levels.py
+++ scripts/plot_storage_levels.py
@@ -96,3 +96,5 @@ if __name__ == "__main__":
 
         figure = plot_storage_levels(df_time_filtered)
         plt.savefig(os.path.join(paths.plotted, region+"_"+timeframe[0][5:7]))
+
+        # Test CI
